dfb2babc10c3537d725d740c3c4f8519dae0f453
Alexandr <furgaylov5@gmail.com>
2020-07-24 16:25:15 +0300

    Initial commit

---
+++ update-deps.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# original script: https://github.com/api-platform/demo/blob/master/update-deps.sh
+
+docker-compose pull
+docker-compose build
+docker-compose run php composer update
+docker-compose run admin /bin/sh -c 'yarn install && yarn upgrade'
+docker-compose run client /bin/sh -c 'yarn install && yarn upgrade'
+cd api && composer sync-recipes --force
+echo 'Run `docker-compose up --build --force-recreate` now and check that everything is fine!'
